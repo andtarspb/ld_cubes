375d5d938f1c6769d7f0d8cd69b9b1849c1cd985
andtarspb <46713402+andtarspb@users.noreply.github.com>
2021-01-13 21:22:03 +0100

    map 1st iteration

---
+++ ld_cubes/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    Transform groundCheck;
+    [SerializeField]
+    float gravity;
+
+    CharacterController controller;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.2f);
+        if (isGrounded && velocity.y < 0)
+        {        
+            velocity.y = -2f;
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 moveInput =  Vector3.ClampMagnitude(transform.right * x + transform.forward * z, 1);
+
+        controller.Move(moveInput * speed * Time.deltaTime);
+
+
+
+    }
+}
